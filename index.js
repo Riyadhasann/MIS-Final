ca61fdc11a634bea4b8e382ddd0e71f4b425a267
Md. Riyad Hasan <hassanriyad666@gmail.com>
2025-01-09 23:04:42 +0600

    6th

---
+++ index.js
@@ -0,0 +1,4 @@
+var searchButton = document.getElementById('searchButton');
+var searchInput = document.getElementById('searchInput');
+var mealsContainer = document.getElementById('mealsContainer');
+var showAllButton = document.getElementById('showAllButton');

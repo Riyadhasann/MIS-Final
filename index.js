ed42a89e8741e84fc142e8009adba099a92c3ba1
Md. Riyad Hasan <hassanriyad666@gmail.com>
2025-01-09 23:20:56 +0600

    7th

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ var searchButton = document.getElementById('searchButton');
 var searchInput = document.getElementById('searchInput');
 var mealsContainer = document.getElementById('mealsContainer');
 var showAllButton = document.getElementById('showAllButton');
+var mealDetailsContent = document.getElementById('mealDetailsContent');
+
+let allMeals = [];
+
+searchButton.addEventListener('click', () => {
+    var query = searchInput.value.trim();
+    if (query) {
+        fetchMeals(query);
+        searchInput.value = '';
+    }
+});
+
+
+searchInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        var query = searchInput.value.trim();
+        if (query) {
+            fetchMeals(query);
+            searchInput.value = '';
+        }
+    }
+});
+
+
+async function fetchMeals(query) {
+    var apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+    try {
+        var response = await fetch(apiUrl);
+        var data = await response.json();
+        displayMeals(data.meals || []);
+    }   catch (error) {
+        console.error('Error fetching meals:', error);
+    }
+}
